2c1367143a0bb27e79c08bbd68b0c8436d563bc5
unknown <tsubasababy1013@gmail.com>
2022-07-29 15:51:26 +0800

    修正更新問題及編號計算

--- api/news.php
+++ api/news.php
@@ -1,27 +1,14 @@
 <?php
 include_once "../base.php";
 
-if(isset($_POST['del'])){
-    foreach($_POST['del'] as $id){
-        $News->del($id);
-    }
-}
-
-$rows=$News->all();
-
-foreach($rows as $row){
-    /* if(in_array($row['id'],$_POST['sh'])){
-     *    $row['sh']=1;
-     * }else{
-     *    $row['sh']=0;
-     * }
-     * 
-     * 上方程式碼簡化成下面 - 
-     * 
-     */
-    $row['sh']=(in_array($row['id'],$_POST['sh']))?1:0;
-
-    $News->save($row);
+foreach($_POST['id'] as $id){
+  if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+      $News->del($id);
+  }else{
+      $row=$News->find($id);
+      $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+      $News->save($row);
+  }
 }
 
 to("../back.php?do=news");
--- back/news.php
+++ back/news.php
@@ -26,11 +26,13 @@
   ?>
   
   <tr>
-    <td><?=$key+1;?></td>
+    <!-- 文章前面的編號 -->
+    <td><?=$now*$div-2+$key;?></td>
     <td><?=$row['title'];?></td>
     <!-- 取得顯示與否的資料並顯示在畫面上 -->
     <td><input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
     <td><input type="checkbox" name="del[]" value="<?=$row['id'];?>" ></td>
+    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
   </tr>
     
   <?php

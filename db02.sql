532265d1f435faa46a13199d5344f2747f84b7bd
unknown <tsubasababy1013@gmail.com>
2022-07-29 16:31:14 +0800

    匯出資料庫檔案

--- db02.sql
+++ db02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-07-29 08:07:11
+-- 產生時間： 2022-07-29 10:30:33
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -53,6 +53,36 @@ INSERT INTO `news` (`id`, `title`, `text`, `type`, `good`, `sh`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `que`
+--
+
+CREATE TABLE `que` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `count` int(11) UNSIGNED NOT NULL,
+  `subject_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `que`
+--
+
+INSERT INTO `que` (`id`, `text`, `count`, `subject_id`) VALUES
+(3, '問題一：你最常做什麼運動來促進健康體能呢?', 0, 0),
+(4, '1.健走或爬樓梯、慢跑等較不受時間、場地限制的運動。', 0, 3),
+(5, '2.仰臥起坐、抬腿及伏地挺身、伸展操、瑜珈等室內運動。', 0, 3),
+(6, '3.球類運動、游泳、跳舞、騎腳踏車等加強心肺功能的運動。', 0, 3),
+(7, '4.舉重鍛鍊、彈力帶、啞鈴等運用輔助器材鍛鍊肌耐力的運動。', 0, 3),
+(8, '問題二：二手菸沒有安全劑量，只要有暴露，就會有危險，請問它會造成身體上哪些危害?', 0, 0),
+(9, '1.增加罹患冠狀動脈心臟病及罹病死亡之風險。', 0, 8),
+(10, '2.對孩子的的健康會產生許多影響，例如容易咳嗽或打噴嚏、罹患氣喘或讓症狀更嚴重、會因刺激耳咽管，感染中耳炎、肺功能較差、容易罹患呼吸道疾病等。', 0, 8),
+(11, '3.孕婦吸入二手菸易造成流產、早產、胎盤早期剝離、子宮感染等疾病。', 0, 8),
+(12, '4.長期的暴露會造成更嚴重的胸腔問題和過敏症，還會增加心臟病和肺癌的罹患率。', 0, 8),
+(13, '5.以上皆是。', 0, 8);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `total`
 --
 
@@ -68,14 +98,7 @@ CREATE TABLE `total` (
 
 INSERT INTO `total` (`id`, `date`, `total`) VALUES
 (1, '2022-07-11', 12),
-(2, '0000-00-00', 1),
-(3, '0000-00-00', 1),
-(4, '0000-00-00', 1),
-(5, '0000-00-00', 1),
-(6, '0000-00-00', 1),
-(7, '0000-00-00', 1),
 (8, '2022-07-28', 1),
-(9, '0000-00-00', 1),
 (10, '2022-07-29', 1);
 
 -- --------------------------------------------------------
@@ -114,6 +137,12 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `que`
+--
+ALTER TABLE `que`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `total`
 --
 ALTER TABLE `total`
@@ -134,6 +163,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `que`
+--
+ALTER TABLE `que`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `total`

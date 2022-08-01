ace3f04a1e6d99527fcd6e46e27b1723064bbc79
unknown <tsubasababy1013@gmail.com>
2022-08-01 09:45:36 +0800

    建立分類網誌基本外觀及點擊功能

--- front/po.php
+++ front/po.php
@@ -1,1 +1,34 @@
-分類網誌
+<style>
+  .type{
+    cursor: pointer;
+    color: blue;
+    margin: 1rem 0;
+    max-width:max-content;
+  }
+  .type:hover{
+    border-bottom:1px solid blue;
+    /* text-decoration: underline; */
+  }
+</style>
+
+<div>目前位置：首頁 > 分類網誌 > <span id="header"></span></div>
+<div style="display: flex;">
+  <fieldset>
+    <legend>分類網誌</legend>
+    <div>健康新知</div>
+    <div>菸害防制</div>
+    <div>癌症防治</div>
+    <div>慢性病防治</div>
+  </fieldset>
+  <fieldset>
+    <legend>文章列表</legend>
+    <div></div>
+  </fieldset>
+</div>
+
+<script>
+  $(".type").on("click",function(){
+    let type=$(this).text()
+    $("#header").text(type)
+  })
+</script>
